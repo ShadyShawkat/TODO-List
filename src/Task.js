3843f6a1dfac5a07d59837b20af90c17aa631ef6
ShadyShawkat <shadyshawkat91@gmail.com>
2021-12-02 00:39:43 +0200

    Fix typo

---
+++ src/Task.js
@@ -0,0 +1,35 @@
+export default class Task {
+
+  static tasks = [
+    {
+      index: 0,
+      description: 'Do chores',
+      completed: false,
+    },
+    {
+      index: 1,
+      description: 'Join board meeting',
+      completed: false,
+    },
+    {
+      index: 2,
+      description: 'Walk the dog',
+      completed: true,
+    },
+  ];
+
+  constructor(description) {
+    this.index = Task.tasks.length;
+    this.description = description;
+    this.completed = false;
+  }
+
+  toggleTaskStatus(index) {
+    Task.tasks = Task.tasks.map(task => {
+      if (task.index == index) {
+        task.completed = !task.completed;
+      }
+      return task;
+    })
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
 import './style.css';
-
-const todoArr = [
-  {
-    index: 0,
-    describtion: 'Do chores',
-    completed: false,
-  },
-  {
-    index: 1,
-    describtion: 'Join board meeting',
-    completed: false,
-  },
-  {
-    index: 2,
-    describtion: 'Walk the dog',
-    completed: true,
-  },
-];
+import Task from './Task'
 
 window.onload = () => {
   const todoList = document.querySelector('.todo-list');
-  todoArr.forEach((todoItem) => {
+  Task.tasks.forEach((todoItem) => {
     const li = document.createElement('li');
     li.className = 'todo-item';
     li.innerHTML = `
-    <label class="${todoItem.completed ? 'todo-completed' : ''}">
+    <label data-index=${todoItem.index} class="${todoItem.completed ? 'todo-completed' : ''}">
       <input type="checkbox" class="todo-item-check" ${todoItem.completed ? 'checked' : ''}>
-      ${todoItem.describtion}
+      ${todoItem.description}
     </label>
     <i class="fas fa-ellipsis-v item-edit-icon"></i>
     `;
