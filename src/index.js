c9ed17389145ab6e9a59421e068e3a0989c32377
ShadyShawkat <shadyshawkat91@gmail.com>
2021-12-01 19:31:53 +0200

    Add list style structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+const todoArr = [
+  {
+    index: 0,
+    describtion: 'Do chores',
+    completed: false
+  },
+  {
+    index: 1,
+    describtion: 'Join board meeting',
+    completed: false
+  },
+  {
+    index: 2,
+    describtion: 'Walk the dog',
+    completed: false
+  },
+];
